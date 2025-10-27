31094b70b982599fa4a2fbc742d3d2c62ffa0eae
Edoardo Paone <edoardo.paone@cscs.ch>
2025-10-27 15:38:23 +0100

    fix test markers

--- tests/next_tests/integration_tests/feature_tests/iterator_tests/test_tuple.py
+++ tests/next_tests/integration_tests/feature_tests/iterator_tests/test_tuple.py
@@ -39,7 +39,7 @@ def tuple_output2(inp1, inp2):
     return make_tuple(deref(inp1), deref(inp2))
 
 
-@pytest.mark.parametrize("stencil", [tuple_output1, tuple_output2])
+@pytest.mark.uses_tuple_iterator
 @pytest.mark.uses_tuple_returns
 def test_tuple_output(program_processor, stencil):
     program_processor, validate = program_processor
@@ -71,6 +71,7 @@ def tuple_of_tuple_output2(inp1, inp2, inp3, inp4):
     return make_tuple(deref(inp1), deref(inp2)), make_tuple(deref(inp3), deref(inp4))
 
 
+@pytest.mark.uses_tuple_iterator
 @pytest.mark.uses_tuple_returns
 def test_tuple_of_tuple_of_field_output(program_processor):
     program_processor, validate = program_processor
@@ -108,6 +109,7 @@ def test_tuple_of_tuple_of_field_output(program_processor):
         assert np.allclose(inp4.asnumpy(), out[1][1].asnumpy())
 
 
+@pytest.mark.uses_tuple_iterator
 @pytest.mark.parametrize("stencil", [tuple_output1, tuple_output2])
 def test_tuple_of_field_output_constructed_inside(program_processor, stencil):
     program_processor, validate = program_processor
@@ -144,6 +146,7 @@ def test_tuple_of_field_output_constructed_inside(program_processor, stencil):
         assert np.allclose(inp2.asnumpy(), out2.asnumpy())
 
 
+@pytest.mark.uses_tuple_iterator
 def test_asymetric_nested_tuple_of_field_output_constructed_inside(program_processor):
     program_processor, validate = program_processor
 
